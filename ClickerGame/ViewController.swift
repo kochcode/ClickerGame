6c795f65998f99ff1c6732f8393b3dce22b1f909
Kevin Koch <kevin.koch23@d155.org>
2022-10-03 08:19:49 -0500

    Initial Commit

---
+++ ClickerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickerGame
+//
+//  Created by KEVIN KOCH on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
