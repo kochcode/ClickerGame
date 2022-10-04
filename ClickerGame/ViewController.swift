2c630012ea9dd0f2b03c9f59fc2d8a1e796c43d9
Kevin Koch <kevin.koch23@d155.org>
2022-10-04 08:11:06 -0500

    button function

--- ClickerGame/ViewController.swift
+++ ClickerGame/ViewController.swift
@@ -8,15 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var clickOutput: UILabel!
+    var multiP : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("test")
     }
     var clicks = 0
+    
     @IBAction func clickInput(_ sender: UIButton) {
-        clicks += 1
+        if multiP == 1{
+            clicks += 1
+        }
+        else if multiP == 2{
+            clicks += 2
+        }
+        else if multiP == 3{
+            clicks += 3
+        }
         clickOutput.text = "Clicks: \(clicks)"
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- ClickerGame/ViewControllerStore.swift
+++ ClickerGame/ViewControllerStore.swift
@@ -11,12 +11,30 @@ class ViewControllerStore: UIViewController {
     
     @IBOutlet weak var storeClickOutput: UILabel!
     var clicksS : Int = 0
-    
+    var multi : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         storeClickOutput.text = "Clicks: \(clicksS)"
         // Do any additional setup after loading the view.
     }
+    @IBAction func timesTwo(_ sender: UIButton) {
+        if clicksS >= 20{
+            clicksS -= 20
+            multi = 2
+        }
+    }
+    @IBAction func timesThree(_ sender: UIButton) {
+        if clicksS >= 80{
+            clicksS -= 80
+            multi = 3
+        }
+    }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "storeSegue"{
+             let clickMain = segue.destination as! ViewController
+            clickMain.clicks = clicksS
+            clickMain.multiP = multi
+        }
+    }
 }
